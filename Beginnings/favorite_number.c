548ca13f79d119e14ff578a4d63633a4f731188e
eneroh <28052446+Eneroh@users.noreply.github.com>
2021-06-20 20:04:24 +1000

    Create favorite_number.c

---
+++ Beginnings/favorite_number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int favNum = 1123;
+    char myChar[] = "letter";
+    printf("My favourite %s is %d and %f", myChar, favNum, 489.089);
+}

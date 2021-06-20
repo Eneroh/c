5f488768727b291132b729b5b22178bd938e3218
eneroh <28052446+Eneroh@users.noreply.github.com>
2021-06-20 19:44:56 +1000

    Create name_age.c

---
+++ Beginnings/name_age.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[] = "Example";
+    int age = 100;
+    printf("My name is %s\n", name);
+    printf("My age is %d\n", age);
+    return 0;
+}

8fc3d0682cbbd815861f5c2a39be326775b98975
eneroh <28052446+Eneroh@users.noreply.github.com>
2021-06-20 20:13:19 +1000

    Create numbers.c

---
+++ Beginnings/numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double num = 5;
+    printf("%.1f\n", 8 / num);
+    printf("%f\n", pow(2,3));
+    printf("%f\n", sqrt(36));
+    printf("%f\n", ceil(36.567));
+    printf("%f\n", floor(36.565));
+    return 0;
+}

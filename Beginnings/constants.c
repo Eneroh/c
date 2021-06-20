ea5590cfb480e9f4796f10dfb1b49b1712b83545
eneroh <28052446+Eneroh@users.noreply.github.com>
2021-06-20 20:23:03 +1000

    Update constants.c

--- Beginnings/constants.c
+++ Beginnings/constants.c
@@ -3,10 +3,13 @@
 
 int main()
 {
-    int num = 5;
+    const int NUM = 5; /* A constant is a number that can't be changed, best practice to uppercase constants */
+    printf("%d", 90) /* This is also considered a constant*/
     printf("%d\n", num);
     num = 8;
     printf("%d", num);
-    
+
     return 0;
 }
+
+/* Running will bring about an error due to an attempt at editing a constant. It will not work. */

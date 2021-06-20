7ae1f04832ae967ee9fe2af6857ead1fa2feef7f
eneroh <28052446+Eneroh@users.noreply.github.com>
2021-06-20 20:23:22 +1000

    Update constants.c

--- Beginnings/constants.c
+++ Beginnings/constants.c
@@ -4,7 +4,7 @@
 int main()
 {
     const int NUM = 5; /* A constant is a number that can't be changed, best practice to uppercase constants */
-    printf("%d", 90) /* This is also considered a constant*/
+    printf("%d", 90) /* This is also considered a constant */
     printf("%d\n", num);
     num = 8;
     printf("%d", num);

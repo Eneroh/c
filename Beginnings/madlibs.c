cee5e0c7e51c4e3c3a957b03bd98489281e4a839
eneroh <28052446+Eneroh@users.noreply.github.com>
2021-07-04 11:40:30 +1000

    Create madlibs.c

---
+++ Beginnings/madlibs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char color[20];
+    char pluralNoun[20];
+    char celebrityF[20];
+    char celebrityL[20];
+    printf("Input a color, plural-noun and celebrity: ");
+    scanf("%s %s %s %s", color, pluralNoun, celebrityF, celebrityL);
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+    printf("I love %s %s", celebrityF, celebrityL);
+
+    return 0;
+}

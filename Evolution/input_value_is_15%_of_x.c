0fe99780ccdf672eda5623f3d9ce7f091b7200c1
eneroh <28052446+eneroh@users.noreply.github.com>
2022-06-17 21:14:53 +1000

    Update input_value_is_15%_of_x.c

--- Evolution/input_value_is_15%_of_x.c
+++ Evolution/input_value_is_15%_of_x.c
@@ -2,29 +2,33 @@
 #include <stdlib.h>
 #include <math.h>
 
-// formula temporarily busted, check back later aha
+// formula fixed, give it a shot will ya?
 
 int main() {
-   int n = 0;
+   int n=0, o=0;
    float p=0.15, f=(float) n, x=0;
    int i=p*100;
-   {
-    printf("Insert value is 15%% of x");
-    printf("\nEnter value: ");
-    scanf("%d", &n);
-    printf("%.2f x %d", p, n);
-    printf("\n%.2f/%.2f*x %d/%.2f", p, p, n, p);
-    printf("\nx = %d*x/%.2f", n, p);
-    printf("\n%d/%.2f", n, p);
-    printf("\n%d/%d", n, i);
-    f=n;
-    printf("\n%0.f", f);
-    printf("\n%d", i);
-    x = f/i;
-    printf("%f", f);
-    printf("%d", i);                                                                                                                                                                                                       
-    printf("\n%.2f", x);
-  }
-  return 0;
+   
+   printf("Insert value is 15%% of x");
+   printf("\nEnter value: ");
+   scanf("%d", &o);
+   n=o;
+   printf("%.2f x %d", p, n);
+   p*n;
+   printf("\n%.2f/%.2f*%d/%.2f", p, p, n, p);
+   p/p*n/p;
+   printf("\nx = %d/%.2f", n, p);
+   n/p;
+   printf("\n%d/%.2f", n, p);
+   n/p;
+   printf("\n%d/%d", n, i);
+   f=n;
+   printf("\n%0.f", f);
+   printf("\n%d", i);
+   x = f/i;
+   printf("\n15%% of %d is: %.2f", o, x);
+   
+   return 0;
 }
-/* Shoutout to my boi, Ant */
+
+/* Shoutout to my boi, Ant */                                                                                                                                       

ccf4515ed446848b514071156d2f7da0f014f230
Eneroh <akinuncoiled@gmail.com>
2022-06-17 16:04:13 +1000

    Addition of wordplay.c files && return of input value 15%.c files

---
+++ Lesson_1/main.s
@@ -0,0 +1,60 @@
+	.file	"main.c"
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Input a color, plural-noun and celebrity: "
+.LC1:
+	.string	"Roses are {color}"
+.LC2:
+	.string	"{plural-noun} are blue"
+.LC3:
+	.string	"I love {celebrity}"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	stdin(%rip), %rdx
+	leaq	-32(%rbp), %rax
+	movl	$20, %esi
+	movq	%rax, %rdi
+	call	fgets@PLT
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L3
+	call	__stack_chk_fail@PLT
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 11.1.0"
+	.section	.note.GNU-stack,"",@progbits

72c53fbf546ff260f3d13587060fce253017f240
eneroh <28052446+eneroh@users.noreply.github.com>
2022-06-17 22:37:59 +1000

    Create input_value_is_input_percentage_of_x.c

---
+++ Evolution/input_value_is_input_percentage_of_x.c
@@ -0,0 +1,33 @@
+  1 #include <stdio.h>
+  2 #include <stdlib.h>
+  3 #include <math.h>
+  4  
+  5 // slightly more functionality than 15% version, slight increase to code too
+  6  
+  7 int main() {
+  8    int n=0, o=0;
+  9    float p=0, f=(float) n, x=0;
+ 10    int i=p*100;
+ 11    
+ 12    printf("\nEnter percentage: ");
+ 13    scanf("%f", &p);
+ 14    printf("\nEnter value: ");
+ 15    scanf("%d", &o);
+ 16    n=o;
+ 17    printf("%.2f x %d", p, n);
+ 18    p*n;
+ 19    printf("\n%.2f/%.2f*%d/%.2f", p, p, n, p);
+ 20    p/p*n/p;
+ 21    printf("\nx = %d/%.2f", n, p);
+ 22    n/p;
+ 23    printf("\n%d/%.2f", n, p);
+ 24    n/p;
+ 25    printf("\n%d/%d", n, i);
+ 26    f=n;
+ 27    printf("\n%0.f", f);
+ 28    printf("\n%d", i);
+ 29    x = f/p;
+ 30    printf("\n%.2f%% of %d is: %.2f", p, o, x);
+ 31    
+ 32    return 0;
+ 33 }      

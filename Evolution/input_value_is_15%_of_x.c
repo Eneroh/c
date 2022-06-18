b94ce181ab1630f8d76f01a844d8c481a8549e6f
eneroh <28052446+eneroh@users.noreply.github.com>
2022-06-18 13:21:25 +1000

    Update input_value_is_15%_of_x.c

--- Evolution/input_value_is_15%_of_x.c
+++ Evolution/input_value_is_15%_of_x.c
@@ -4,30 +4,31 @@
 
 // formula fixed, give it a shot will ya?
 
-int main() {
-   float p=0.15, f=0, x=0;
-   int i=p*100, n=0, o=0;
-   
-   printf("Insert value is 15%% of x");
-   printf("\nEnter value: ");
-   scanf("%d", &o);
-   n=o;
-   printf("%.2f x %d", p, n);
-   p*n;
-   printf("\n%.2f/%.2f*%d/%.2f", p, p, n, p);
-   p/p*n/p;
-   printf("\nx = %d/%.2f", n, p);
-   n/p;
-   printf("\n%d/%.2f", n, p);
-   n/p;
-   printf("\n%d/%d", n, i);
-   f=n;
-   printf("\n%0.f", f);
-   printf("\n%d", i);
-   x = f/i;
-   printf("\n15%% of %d is: %.2f", o, x);
-   
-   return 0;
-}
-
-/* Shoutout to my boi, Ant */                                                                                                                                       
+int main() {                                                                                                                                                            
+   float p=0.15, f=0, x=0;    
+   int i=p*100, n=0, o=0;    
+       
+   printf("Insert value is 15%% of x");    
+   printf("\nEnter value: ");    
+   scanf("%d", &o);    
+   n=o;    
+   printf("%.2f x %d", p, n);    
+   p*n;    
+   printf("\n%.2f/%.2f*%d/%.2f", p, p, n, p);    
+   p/p*n/p;    
+   printf("\nx = %d/%.2f", n, p);    
+   n/p;    
+   printf("\n%d/%.2f", n, p);    
+   n/p;    
+   printf("\n%d/%d", n, i);    
+   f=n;    
+   //printf("\n%0.f", f);    
+   //printf("\n%d", i);    
+   x = f/i;    
+   printf("\nYour answer is: %.2f", x);    
+   printf("\n15 times %.2f = %d", x, o);    
+       
+   return 0;    
+}    
+    
+/* Shoutout to my boi, Ant */  
